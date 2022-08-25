a55890a5dcdf8ae56da9be4fc3eaf0cf33fb755f
ray dolber <ray@stills.io>
2022-08-24 22:09:10 -0700

    Initial Commit

---
+++ Project-Take-Home/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project-Take-Home
+//
+//  Created by Ray Dolber on 8/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
