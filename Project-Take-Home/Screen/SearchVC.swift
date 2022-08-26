090b2dde9529176c612514462baeea4351a07644
ray dolber <ray@stills.io>
2022-08-26 13:12:14 -0700

    Custom Alert

--- Project-Take-Home/Screen/SearchVC.swift
+++ Project-Take-Home/Screen/SearchVC.swift
@@ -36,7 +36,7 @@ class SearchVC: UIViewController {
     
     @objc func pushFollwerListVC() {
         guard isUserNameEntered else {
-            print("NO USERNAME")
+            presentGFAlertOnMainThread(title: "Empty Username", message: "Please enter a username. We need to know who to look for.😀", buttonTitle: "Ok")
             return
         }
         
