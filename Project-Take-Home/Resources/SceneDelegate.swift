4a5c796f00ac149bbd29923a9339a5ff49247697
ray dolber <ray@stills.io>
2022-08-25 18:53:48 -0700

    pass data between screens

--- Project-Take-Home/Resources/SceneDelegate.swift
+++ Project-Take-Home/Resources/SceneDelegate.swift
@@ -26,6 +26,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window?.windowScene = windowScene
         window?.rootViewController = createTabBar()
         window?.makeKeyAndVisible()
+        
+        configureNavigationBar()
+    }
+    
+    func configureNavigationBar() {
+        UINavigationBar.appearance().tintColor = .systemGreen
     }
     
     func createSearchNC() -> UINavigationController {
