bbd35af9c0bf835ae50dc72464b3a64adf5a4a75
ray dolber <ray@stills.io>
2022-08-27 01:19:55 -0700

    Use new result type from Swift5

--- Project-Take-Home/Screens/FollowerListVC.swift
+++ Project-Take-Home/Screens/FollowerListVC.swift
@@ -17,14 +17,24 @@ class FollowerListVC: UIViewController {
         
         navigationController?.navigationBar.prefersLargeTitles  = true
         
-        NetworkManager.shared.getFollowers(for: username, page: 1) { followers, errorMessage in
-            guard let followers = followers else {
-                self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: errorMessage!.rawValue, buttonTitle: "OK")
-                return
+//        NetworkManager.shared.getFollowers(for: username, page: 1) { followers, errorMessage in
+//            guard let followers = followers else {
+//                self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: errorMessage!.rawValue, buttonTitle: "OK")
+//                return
+//            }
+//
+//            print("followers.count : ", followers.count)
+//            print(followers)
+//        }
+        
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            switch result {
+            case .success(let followers):
+                print("followers.count : ", followers.count)
+                print(followers)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "OK")
             }
-            
-            print("followers.count : ", followers.count)
-            print(followers)
         }
     }
     
